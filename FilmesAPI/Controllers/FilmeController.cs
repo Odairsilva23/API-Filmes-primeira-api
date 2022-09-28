0ab12baba38a518b321b10efa3db81202369da1b
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2022-09-28 11:32:52 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,50 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController] // classe para criar a API
+    [Route("[controller]")] // define q a rota para acessa a API será sempre 'nome' + 'Controller', ou seja, FilmeController
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
+        // palavra reservada do método Rest, que significa CRIAR alguma coisa
+        [HttpPost] // estou dizendo que a inf enviada pelo Programa Postman vai postar a info no server da minha API
+        public IActionResult AdicionaFilme([FromBody] Filme filme) // recebe a inf enviada pelo postman pelo seu "Body"
+        {
+            filme.Id = Id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);
+        }
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmePorId(int id)
+        {
+            //foreach (Filme filme in filmes)
+            //{
+            //    if (filme.Id == id)
+            //    {
+            //        return filme;
+            //    }
+            //}
+            //return null;
+
+           Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Titulo é obrigatório !")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório !")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(25, 300, ErrorMessage = "A Duração do deve estar entre 25 minutos e 300 minutos")]
+        public int Duracao { get; set; }
+        
+    }
+}
