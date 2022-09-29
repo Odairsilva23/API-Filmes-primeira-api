f3d0d72837115abb0493cd7eb94cec964c8c8b20
Odair (Tio Daia) <odair.tiodaia95@gmail.com>
2022-09-29 08:19:35 -0300

    Feita Alteração , adicionado DB feito teste de persistencia

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,24 +8,29 @@ using System.Threading.Tasks;
 
 namespace FilmesAPI.Controllers
 {
+   
     [ApiController] // classe para criar a API
     [Route("[controller]")] // define q a rota para acessa a API será sempre 'nome' + 'Controller', ou seja, FilmeController
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int Id = 1;
+        private FilmeContext _context;
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+       
         // palavra reservada do método Rest, que significa CRIAR alguma coisa
         [HttpPost] // estou dizendo que a inf enviada pelo Programa Postman vai postar a info no server da minha API
         public IActionResult AdicionaFilme([FromBody] Filme filme) // recebe a inf enviada pelo postman pelo seu "Body"
         {
-            filme.Id = Id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);
         }
         [HttpGet]
-        public IActionResult RecuperarFilmes()
+        public IEnumerable<Filme> RecuperarFilmes()
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
         [HttpGet("{id}")]
         public IActionResult RecuperarFilmePorId(int id)
@@ -38,7 +44,7 @@ namespace FilmesAPI.Controllers
             //}
             //return null;
 
-           Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+           Filme filme = _context.Filmes.FirstOrDefault(filmes => filmes.Id == id);
             if (filme != null)
             {
                 return Ok(filme);
